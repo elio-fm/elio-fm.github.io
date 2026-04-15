cd7e87b59a86aa6b76fb5a13b13342580f70dd76
MiguelRegueiro <miguelpr4242@gmail.com>
2026-04-16 00:15:49 +0200

    feat(seo): improve homepage metadata and indexing setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from "astro/config";
+import { copyFile } from "node:fs/promises";
 import sitemap from "@astrojs/sitemap";
+
+const stableSitemap = {
+  name: "stable-sitemap",
+  hooks: {
+    "astro:build:done": async ({ dir }) => {
+      await copyFile(new URL("./sitemap-index.xml", dir), new URL("./sitemap.xml", dir));
+    },
+  },
+};
 
 export default defineConfig({
   site: "https://elio-fm.github.io",
   output: "static",
-  integrations: [sitemap()],
+  integrations: [sitemap(), stableSitemap],
 });
